180e744b3c0e1fdf90eb34644f61b942d92fb465
Nathan Brown <nkabrown@gmail.com>
2018-02-13 21:11:51 -0500

    update episode source code
    * implement current Elm architecture
    * replace Random.generate with Random.step
    * generate a 'seed' of randomness with current time

--- elmseeds/elmseed-001/Core.elm
+++ elmseeds/elmseed-001/Core.elm
@@ -1,26 +1,34 @@
-module Core where
+port module Core exposing (..)
 
 import Array exposing (Array)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Random exposing (Seed)
-import StartApp.Simple as StartApp
 
+port dealOne : (Bool -> msg) -> Sub msg
+
+port sendToJs : Int -> Cmd msg
 
 main =
-  StartApp.start { model = model, view = view, update = update }
+  Html.program { init = init, view = view, update = update, subscriptions = (\_ -> dealOne DealOneCard) }
 
 
-type Action = FetchNextCard
+type alias Model = { card : Maybe Card, remainingCards: List Card }
 
 
-model : { card: Maybe Card, remainingCards: List Card }
-model =
-  { card = Nothing, remainingCards = shuffledDeck }
+init : (Model, Cmd msg)
+init =
+  ({ card = Nothing, remainingCards = shuffledDeck 0 }, Cmd.none)
 
 
-view address model =
+type Msg =
+   FetchNextCard
+   | DealOneCard Bool
+
+
+view : Model -> Html Msg
+view model =
   div [class "container"]
     [ div [class "row"]
         [ div [class "twelve columns"]
@@ -29,28 +37,34 @@ view address model =
         ]
     , div [class "row"]
         [ div [class "one-third column"]
-            [ button [onClick address FetchNextCard ] [ text "Next Card" ] ]
+            [ button [onClick FetchNextCard ] [ text "Next Card" ] ]
         ]
     ]
 
 
-update action model =
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
   let
     newCard = List.head model.remainingCards
     remainingCards = Maybe.withDefault [] (List.tail model.remainingCards)
   in
-    case action of
-      FetchNextCard -> { model | card = newCard, remainingCards = remainingCards }
+    case msg of
+      FetchNextCard ->
+        ({ model | card = newCard, remainingCards = remainingCards }
+        , sendToJs <| List.length remainingCards)
+      DealOneCard _ ->
+        ({ model | card = newCard, remainingCards = remainingCards }, Cmd.none)
 
 
-shuffledDeck =
+shuffledDeck : Int -> List Card
+shuffledDeck time =
   let
     suits = [Clubs, Diamonds, Hearts, Spades]
     cardsWithSuit s =
       List.map (\c -> c s)
         [Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two]
     fullDeck = Array.fromList (List.concatMap cardsWithSuit suits)
-    seed = Random.initialSeed 1
+    seed = Random.initialSeed time
   in
     Array.toList (shuffle fullDeck 0 seed)
 
@@ -59,7 +73,7 @@ shuffle : Array Card -> Int -> Seed -> Array Card
 shuffle unshuffled i seed =
   let
     g = Random.int 0 ((Array.length unshuffled) - i - 1)
-    (j, nextSeed) = Random.generate g seed
+    (j, nextSeed) = Random.step (Random.int 0 1) seed
     atI = Maybe.withDefault InvalidCard (Array.get i unshuffled)
     atIJ = Maybe.withDefault InvalidCard (Array.get (i + j) unshuffled)
     shuffled = Array.set i atIJ unshuffled |> Array.set (i + j) atI
@@ -68,6 +82,7 @@ shuffle unshuffled i seed =
       shuffled
     else
       shuffle shuffled (i + 1) nextSeed
+
 
 type Card
   = InvalidCard
